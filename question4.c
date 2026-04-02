ed1dc558810f31f52a1b4d36377fc6dcd8c2d9ec
Jhessyca Borges <jhessyca.borgez@outlook.com>
2026-04-02 15:27:27 -0300

    Adicionando a questão 4 da avaliação

---
+++ question4.c
@@ -0,0 +1,45 @@
+// Questão 4 - Simulação de Caixa Eletrônico
+
+#include <stdio.h>
+
+int main() {
+	float saldo, valor;
+	int opcao;
+	
+	printf("Digite o saldo inicial: ");
+	scanf("%f", &saldo);
+	
+	do {
+		printf("\n--- MENU CAIXA ELETRONICO ---\n");
+		printf("1 - Consultar saldo\n");
+		printf("2 - Sacar valor\n");
+		printf("3 - Depositar valor\n");
+		printf("4 - Sair\n");
+		printf("Escolha uma opcao: ");
+		scanf("%d", &opcao);
+		
+		if (opcao == 1) {
+			printf("Saldo atual: %.2f\n", saldo);
+		} else if (opcao == 2) {
+			printf("Digite o valor do saque: ");
+			scanf("%f", &valor);
+			if(valor > saldo) {
+				printf("Saldo insuficiente.\n");
+			} else {
+				saldo = saldo - valor;
+				printf("Saque realizado!\n");
+			}
+		} else if (opcao == 3) {
+			printf("Digite o valor do deposito: ");
+			scanf("%f", &valor);
+			saldo = saldo + valor;
+			printf("Deposito realizado!\n");
+		} else if (opcao == 4) {
+			printf("Saindo... Ate logo!\n");
+		} else {
+			printf("Opcao invalida!\n");
+		}
+	} while (opcao != 4);
+	
+	return 0;
+}
